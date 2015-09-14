73d0520ebd28f3ad399dcfff0e43f57edf14c7e3
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2015-09-14 21:21:54 +0200

    WIP

--- src/js/server/api/bin/evaluate.sh
+++ src/js/server/api/bin/evaluate.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
+# Returns the number with explicit + sign if it's non-negative
+with_signum() {
+    SIGN=
+    if [ "$1" -ge 0 ]; then
+        SIGN=+
+    fi
+
+    echo "$SIGN$1"
+}
+
 SRC=$1
-CELL=$2
+DEST=$2
 X=$3
 Y=$4
+R=2
 
-X0=$(echo "$X-$CELL/2" | bc)
-Y0=$(echo "$Y-$CELL/2" | bc)
-X1=$(echo "$X+$CELL/2" | bc)
-Y1=$(echo "$Y+$CELL/2" | bc)
+RR=$(expr $R \* 2)
+X0=$(expr $X \- $R)
+Y0=$(expr $Y \- $R)
+
+CROPW=$RR
+CROPH=$RR
+CROPX=$(with_signum $X0)
+CROPY=$(with_signum $Y0)
 
 # SED matching stuff
 NUM='\([0-9]\{1,\}\)'
-FORMAT='{"x":\1,"y":\2,"v":[\3,\4,\5]}'
+FORMAT='\3,\4,\5'
 REGEX="s/^$NUM,$NUM: ($NUM,$NUM,$NUM).*/$FORMAT/g"
-CROP="${CELL}x${CELL}+$X0+$Y0"
 
-echo $CROP
+# Crop geometry
+CROP="${CROPW}x${CROPH}$CROPX$CROPY"
 
 convert $SRC \
     -colorspace RGB  \
-    -crop "${CELL}x${CELL}+$X0+$Y0" +repage
+    -crop $CROP +repage \
     -scale 1x1 \
     text:- | tail -n +2 | sed -e "$REGEX"
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Download first
-./src/js/server/api/bin/download.sh http://www.shmu.sk/data/datanwp/zrazky/zrazky_15092015-09.png cache/data.png
+# time ./src/js/server/api/bin/download.sh http://www.shmu.sk/data/datanwp/zrazky/zrazky_15092015-09.png downloaded.png
 
-time ./src/js/server/api/bin/evaluate.sh cache/data.png 4 20 20
+time ./src/js/server/api/bin/evaluate.sh test.jpg cropped.jpg 200 200
